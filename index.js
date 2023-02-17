600afabdb572e5adabfefd7be3afe4f99c2bfff4
NikJangra <nikhilkumar121288@gmail.com>
2023-02-17 12:09:36 +0530

    CICD working

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express  = require('express');
+
+const app = express();
+
+
+
+app.get("/",(req,res)=>{
+    res.send(" Yaa its working !!")
+})
+
+
+app.listen(8000,()=>{
+    console.log('listening on 8000')
+})
